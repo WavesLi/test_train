fb63dd42e408e842b916fc53273701a4862718dc
WavesLi <33248674+WavesLi@users.noreply.github.com>
2018-04-02 23:48:59 +0800

    Update get_freq_train.py

--- get_freq_train.py
+++ get_freq_train.py
@@ -1,6 +1,6 @@
-with open('Data/test.csv','r',encoding='utf-8') as f1:
-    with open('Data/trans_test.csv','r',encoding='utf-8') as f2:
-        with open('Data/freq_test.csv','w',encoding='utf-8') as f3:
+with open('Data/train.csv','r',encoding='utf-8') as f1:
+    with open('Data/trans_train.csv','r',encoding='utf-8') as f2:
+        with open('Data/freq_train.csv','w',encoding='utf-8') as f3:
             f1.readline()
             f2.readline()
             f3.write('ip_freq,app_freq,device_freq,os_freq,channel_freq,ip_app_freq,ip_device_freq,ip_channel_freq,app_channel_freq,ip_app_channel_freq,hour_freq,MM_freq,lable'+'\n')
@@ -18,6 +18,7 @@ with open('Data/test.csv','r',encoding='utf-8') as f1:
                     for i,item in enumerate(line2):
                         if i%2==1:
                             line.append(item)
-                    if number == 5:
+                    line.append(line1[-1])
+                    if number == 5 or line1[-1] == '1':
                         f3.write(','.join(line)+'\n')
                         number = 0
